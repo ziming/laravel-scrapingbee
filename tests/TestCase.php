a01575391310032563734f7e46b2526a8b783e33
Ziming <ziming.lan@gmail.com>
2021-09-05 20:35:31 +0800

    Remove redundant code in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,12 +11,6 @@ class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
-
-        /*
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Ziming\\LaravelScrapingBee\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-        */
     }
 
     protected function getPackageProviders($app)
@@ -29,10 +23,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_laravel-scrapingbee_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }
