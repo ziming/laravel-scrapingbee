976c04ce0be02e4b0fb51a506e2e88db491084f7
ziming <ziming.opensource@gmail.com>
2023-02-17 00:56:24 +0800

    Support Laravel 10

--- rector.php
+++ rector.php
@@ -1,22 +1,19 @@
 <?php
 
 declare(strict_types=1);
-
-use Rector\Core\Configuration\Option;
-use Rector\Laravel\Set\LaravelSetList;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
+return static function (RectorConfig $rectorConfig): void {    // get parameters
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+    ]);
     // $containerConfigurator->import(LaravelSetList::LARAVEL_80);
 
     // get services (needed for register a single rule)
